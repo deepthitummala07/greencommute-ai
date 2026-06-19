1ec83f15ac713c028822d18ff544e3d495a4e94e
deepthitummala07 <deepthi.tummala07@gmail.com>
2026-06-19 11:03:10 +0530

    Improved homepage UI

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 function predictDemand(){
 
     document.getElementById("result").innerHTML = `
-        <h3>Prediction Result</h3>
+        <h3>Demand Prediction</h3>
 
-        Demand Level: High <br><br>
+        <p><b>Demand Level:</b> HIGH 🔴</p>
 
-        Recommendation:
+        <h4>Recommendations</h4>
+
         <ul>
             <li>Add 2 Extra Buses</li>
             <li>Increase Metro Frequency</li>
-            <li>Send Crowd Alert</li>
+            <li>Send Crowd Alerts</li>
         </ul>
+
+        <h4>Sustainability Impact</h4>
+
+        <ul>
+            <li>CO₂ Saved: 1.4 kg</li>
+            <li>Fuel Saved: 0.6 L</li>
+        </ul>
+
+        <h4>Green Rewards</h4>
+
+        <p>⭐ +15 Green Points</p>
     `;
 }
