1d0e05c3dce4f4ad008491ae345afc0d466b0c18
Deepthi Tummala <deepthi.tummala07@gmail.com>
2026-06-19 10:06:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function predictDemand(){
+
+    document.getElementById("result").innerHTML = `
+        <h3>Prediction Result</h3>
+
+        Demand Level: High <br><br>
+
+        Recommendation:
+        <ul>
+            <li>Add 2 Extra Buses</li>
+            <li>Increase Metro Frequency</li>
+            <li>Send Crowd Alert</li>
+        </ul>
+    `;
+}
